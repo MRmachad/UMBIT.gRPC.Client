96970b856a7c14dfe9ecda51a823163c0068e885
Leonardo Machado <machadoleonardo7856@gmail.com>
2023-07-14 14:04:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Grpc.Net.Client;
+using UMBIT.gRPC.Serve;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var channel = GrpcChannel.ForAddress("https://localhost:7105");
+        var client =  new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(
+                        new HelloRequest { Name = "Leonardo Machado" });
+        Console.WriteLine("Saudacao: " + reply.Message);
+        Console.WriteLine("Pressione qualquer coisa para sair...");
+        Console.ReadKey();
+    }
+}
